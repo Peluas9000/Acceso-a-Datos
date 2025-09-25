3cd3d3118b2a16b56c47299a073cced615685e83
alumno <alumno@DAM218>
2025-09-25 08:51:01 +0200

    Ejercicio1

---
+++ AccesoDatos/src/ficheros/tarea2/ej1/GestionFicheroA.java
@@ -0,0 +1,37 @@
+package ficheros.tarea2.ej1;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class GestionFicheroA {
+	public static void main(String[] args) {
+		try {
+			FileReader fr=new FileReader("Ejercicio1.txt");
+			BufferedReader br=new BufferedReader(fr);
+			
+			String cadena;
+			
+			try {
+				while((cadena=br.readLine())!=null) {
+					System.out.println(cadena);
+				}
+				br.close();
+				fr.close();
+				
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
